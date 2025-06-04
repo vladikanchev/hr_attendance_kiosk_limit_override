8368bccc37cdde31725af02f9c8b6957beea34dd
Vladimir Kanchev <95029070+vladikanchev@users.noreply.github.com>
2025-06-04 13:36:32 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "version": "1.0",
     "category": "Human Resources",
     "summary": "Limits Kiosk Mode Check-in and Check-out by time",
-    "author": "Custom",
+    "author": "Vladi",
     "depends": ["hr_attendance"],
     "data": [],
     "installable": True,
